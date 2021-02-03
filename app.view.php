ed4229dd669ec96ae86b82a6028d1b4be6584494
Ayub <ayubali8448@icloud.com>
2021-02-03 18:16:35 -0500

    first commit

---
+++ app.view.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://unpkg.com/@popperjs/core@2"></script>
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <title>Lab 2</title>
+</head>
+
+<body>
+<!-- Header of the webpage Title of site -->
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Navbar</a>
+            </div>
+        </nav>
+    </header>
+    <!-- Main body of the site photo gallary -->
+</body>
+
+</html>
